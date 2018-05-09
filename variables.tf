4cad80f26cf85421a80c0efb506a738097c9e0f2
Jihwan Chun <jihwan.chun@bluehole.net>
2018-05-09 17:23:40 +0900

    Do not set region in the module - provider will be injected where the module is used.

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "aws_region" {
-  description = "Region where Cloud Formation is created"
-  default     = "eu-central-1"
-}
-
 variable "cluster_name" {
   description = "Name of the AWS Minikube cluster - will be used to name all created resources"
 }
